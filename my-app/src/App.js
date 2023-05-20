978ae2b5f25c2960f0f6f50995f56285cb4e6298
Glenn Chen <glenn.jun.chen@gmail.com>
2023-05-20 15:50:34 -0700

    merge

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PredictionResult from './components/PredictionResult.js';
+
+function App() {
+  return (
+    <div>
+      <PredictionResult />
+    </div>
+  );
+}
+
+export default App;
